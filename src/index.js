f006fe181136ab7271f8ca6d0a999c3a18033709
chamipon <zkucera14@gmail.com>
2021-08-27 12:51:27 -0400

    test

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const P = new Pokedex.Pokedex({
 	cacheImages: true,
 	timeout: 5000,
 });
-
+console.log("test")
 (async () => {
 	const pokes = await P.getPokemonByName([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]);
 	const pokeItems = pokes.map((poke) => {
