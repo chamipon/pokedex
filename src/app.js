274a1afd4759a55809b4c4294f92d84bff2d850d
chamipon <zkucera14@gmail.com>
2021-10-31 01:22:00 -0400

    lazy load cards

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-import * as pokeFuncs from "./pokeFuncs.js";
-import * as helpers from "./helpers.js";
 import PokeCard from "./pokeCard/pokeCard";
 import Navbar from "./navbar/navbar";
+import LazyLoad from 'react-lazyload';
 import React, { useState, useEffect } from "react";
 import "./app.css";
 function App(props) {
-	const [pokes, setPokes] = useState(""); // All the pokemon data that has been fetched.
-	const [displayPokes, setDisplayPokes] = useState(""); //The pokemon that are currently being displayed.
+	const [pokes, setPokes] = useState(""); // Master list of every pokemon. Only contains name and url to species, fully populated at the start
+	const [pokeObjs, setPokeObjs] = useState([]	) // All the pokemon objects that have been fetched. Populated as data is needed
 	useEffect(() => {
 		
 		const Pokedex = require("pokeapi-js-wrapper");
@@ -14,43 +13,26 @@ function App(props) {
 			cacheImages: true,
 			timeout: 10000,
 		});
-		var monarray = []
-		var i = 1;
-		while(i<30) monarray.push(i++)
-		console.log(monarray)
-		monarray.push(143);
-		// monarray.push(265);
-		monarray.push(280);
-		// monarray.push(439);
-		monarray.push(415);
-		// monarray.push(470);
-		// monarray.push(471);
-		// monarray.push(196);			
-		// monarray.push(197);
-		monarray.push(133);
-		monarray.push(616);
-		// monarray.push(850);
-		P.getPokemonByName(monarray).then((info) => {
-			setPokes(info);
-			setDisplayPokes(info);
-		});
+		P.getPokemonSpeciesList().then((info) =>{ //Pulls the name and url to species for every pokemon.
+			setPokes(info.results)
+		})
 	}, []);
 
 	return (
 		<div id="modeContainer" className="dark">
 			<div className="scrollContainer">
 				<h1 className="text-center">Ultradex</h1>
-				<div id="PokeGrid" className="mx-auto container row">{displayPokes && displayPokes.map((poke,i) => (
-						<PokeCard
-							key={poke.id}
-							index={i}
-							number={poke.id}
-							name={helpers.capitalize(pokeFuncs.getPokeName(poke))}
-							sprite={poke.sprites.front_default}
-							poke={poke}
-							pokeList={pokes}
-							pokeListUpdater={setPokes}
-						/>
+				<div id="PokeGrid" className="mx-auto container row">{pokes && pokes.map((poke,i) => (
+						<LazyLoad className="col-12 col-sm-6 col-lg-4" scrollContainer=".scrollContainer" height={98} once >
+							<PokeCard
+								key={i}
+								number={i+1}
+								index={i}
+								poke={poke}
+								pokeList={pokeObjs}
+								pokeListUpdater={setPokeObjs}
+							/>
+						</LazyLoad>
 					)
 				)}</div>
 				<button className="btn btn-secondary mx-auto">Load More</button>
--- src/pokeCard/pokeCard.js
+++ src/pokeCard/pokeCard.js
@@ -1,12 +1,15 @@
 import "./pokeCard.css";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import $ from "jquery";
-
+import * as pokeFuncs from "./../pokeFuncs.js";
+import * as helpers from "./../helpers.js";
 import EvoChain from "./evoChain/evoChain";
+import LazyLoad from 'react-lazyload';
 
 function PokeCard(props) {
 	const [evoChain, setEvoChain] = useState(""); //The evolution chain for the pokemon.
 	const [species, setSpecies] = useState(""); //The species for the pokemon.
+	const [poke, setPoke] = useState(""); //The pokemon object.
 	const [expanded, setExpanded] = useState(false); //The species for the pokemon.
 	const Pokedex = require("pokeapi-js-wrapper");
 	const P = new Pokedex.Pokedex({
@@ -14,45 +17,84 @@ function PokeCard(props) {
 		timeout: 5000,
 	});
 	useEffect(() => {
-		P.getPokemonSpeciesByName(props.number).then((info) => {
-			setSpecies(info)
-			setEvoChain(info.evolution_chain.url)
-		})
-		
-
-	}, []);
-	function expandCard(e){
-
-		var button = $(e.currentTarget).parent('.pokeCard'); // The button clicked on
-		var cardPerRow = 1; //The number of pokeCards per row, changes based on screen width.
-		if(window.innerWidth >= 992) cardPerRow = 3;
-		else if(window.innerWidth >= 576) cardPerRow = 2;
-		
-		var offset = button.attr("data-index") % cardPerRow; //The number of button widths the card-body needs to be shifted over. 
-		
-		if($(button).hasClass('expanded')) $(button).removeClass("expanded") //if the card clicked on was the card that is open, close the card.
-		else {//If the card clicked was a different card, close the current one, open the new one.
-			$("#PokeGrid").find('.expanded').removeClass('expanded')
-			$(button).addClass("expanded")
-			$(button).find(".card-body").css('width', $('#PokeGrid').width() - 24) //Set the width of the card-body.
-			$(button).find(".card-body").css('right', offset * ($(button).width() + 24)) //Shift the card-body based on the card's position in the row. 
+		async function fetchData() {
+			var spec = await P.resource(props.poke.url);
+			setSpecies(spec);
+			setEvoChain(spec.evolution_chain.url);
+			var pokeObj = await P.getPokemonByName(spec.name);
+			setPoke(pokeObj);
 		}
-		if(!expanded) setExpanded(true)
+		fetchData();
+	}, []);
+	function expandCard(e) {
+		var button = $(e.currentTarget).parent(".pokeCard"); // The button clicked on
+		var cardPerRow = 1; //The number of pokeCards per row, changes based on screen width.
+		if (window.innerWidth >= 992) cardPerRow = 3;
+		else if (window.innerWidth >= 576) cardPerRow = 2;
+
+		var offset = button.attr("data-index") % cardPerRow; //The number of button widths the card-body needs to be shifted over.
+
+		if ($(button).hasClass("expanded")) $(button).removeClass("expanded");
+		//if the card clicked on was the card that is open, close the card.
+		else {
+			//If the card clicked was a different card, close the current one, open the new one.
+			$("#PokeGrid").find(".expanded").removeClass("expanded");
+			$(button).addClass("expanded");
+			$(button)
+				.find(".card-body")
+				.css("width", $("#PokeGrid").width() - 24); //Set the width of the card-body.
+			$(button)
+				.find(".card-body")
+				.css("right", offset * ($(button).width() + 24)); //Shift the card-body based on the card's position in the row.
+		}
+		if (!expanded) setExpanded(true);
 	}
 
 	return (
-		<div className="col-12 col-sm-6 col-lg-4">
-			<div data-index={props.index} className="card pokeCard w-100">
-				<div onClick={(e) => expandCard(e)} role="button" className="card-header">
+		<div data-index={props.index} className="card pokeCard w-100">
+			{poke && (
+				<div
+					onClick={(e) => expandCard(e)}
+					role="button"
+					className="card-header"
+				>
 					<div className="pokeSprite">
-						<img className="h-100" src={props.sprite} alt={props.name + "Sprite"} />
+						<img
+							className="h-100"
+							src={poke && poke.sprites.front_default}
+							alt={
+								poke &&
+								helpers.capitalize(
+									pokeFuncs.getPokeName(poke)
+								) + "Sprite"
+							}
+						/>
 					</div>
-					<span className="pokeName m-auto">#{props.number} {props.name}</span>
+					<span className="pokeName m-auto">
+						#{props.number}{" "}
+						{poke &&
+							helpers.capitalize(pokeFuncs.getPokeName(poke))}
+					</span>
 				</div>
+			)}
+			{poke && (
 				<div className="card-body">
-					<EvoChain render={expanded} key={props.name + "_evoChain"} poke={props.poke} pokeList={props.pokeList} pokeListUpdater={props.pokeListUpdater} chain={evoChain}/>
+					<LazyLoad>
+					<EvoChain
+						render={expanded}
+						key={
+							poke &&
+							helpers.capitalize(pokeFuncs.getPokeName(poke)) +
+								"_evoChain"
+						}
+						poke={poke}
+						pokeList={props.pokeList}
+						pokeListUpdater={props.pokeListUpdater}
+						chain={evoChain}
+					/>
+					</LazyLoad>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
