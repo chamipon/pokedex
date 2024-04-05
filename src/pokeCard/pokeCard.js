a0988c894650fc11ed997bed4224b2ebe284a23c
chamipon <zkucera14@gmail.com>
2024-04-04 20:53:17 -0400

    use next/image for pokecards

--- src/pokeCard/pokeCard.js
+++ src/pokeCard/pokeCard.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import * as helpers from "./../helpers.js";
-import LazyLoad from 'react-lazyload';
 import SettingsContext from "../../contexts/settings.js";
 import Link from 'next/link'
+import Image from 'next/image';
 import styles from "./pokeCard.module.scss"
 function PokeCard(props) {
     const [settings] = useContext(SettingsContext)
@@ -14,15 +14,18 @@ function PokeCard(props) {
 						<a className={styles.cardHeader + ' card-header'}>
                             
                                 <div className={styles.pokeSprite}>
-                                    {settings.fetched && <img 
-                                        src = {(settings.useArt 
-                                            ? "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+props.number+".png"
-                                            : (settings.isShiny 
-                                                ? "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+props.number+".png" 
-                                                : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+props.number+".png")
-                                            )}
-                                        alt={helpers.capitalize(props.name) + "Sprite"}
-                                    />}
+                                    {settings.fetched && 
+                                        <Image 
+                                            src = {(settings.useArt 
+                                                ? "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+props.number+".png"
+                                                : (settings.isShiny 
+                                                    ? "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+props.number+".png" 
+                                                    : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+props.number+".png")
+                                                )}
+                                            height={96}
+                                            width={96}
+                                            alt={helpers.capitalize(props.name) + "Sprite"}
+                                        />}
                                 </div>
                             
                             <span className={"m-auto"}>
